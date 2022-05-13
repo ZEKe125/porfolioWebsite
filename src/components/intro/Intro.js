58ccc3bb7858c84d36b7a1941d9fd685771fbaaa
ZEKe125 <82134360+ZEKe125@users.noreply.github.com>
2022-05-12 20:47:30 -0700

    profile

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -17,6 +17,8 @@ import {
   FaReact,
 } from 'react-icons/fa';
 import { Flex, Spacer } from '@chakra-ui/react';
+import profile from '../../assets/profile.png';
+
 // import mySvg from '../../../src/assets/intro_bg.svg';
 
 function Intro() {
@@ -28,29 +30,43 @@ function Intro() {
         </Text>
       </div>
 
-      <SimpleGrid columns={2} spacing={10} className="grid">
+      <SimpleGrid columns={2} spacing={10}>
         <Box>
-          <a href="#">
-            <Image
-              className="img"
-              objectFit="scale-down"
-              src="https://caltrout.org/wp-content/uploads/2019/08/UC-merced-logo.png"
-              alt="react.js"
-            />
-          </a>
-          <a href="#">
-            <Image
-              className="img"
-              objectFit="scale-down"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/UC_Merced_logo.svg/1200px-UC_Merced_logo.svg.png"
-              alt="redux.js"
-            />
-          </a>
+          <Center>
+            <a
+              className="profile"
+              href="https://engineering.ucmerced.edu/"
+              target={'_blank'}
+            ></a>
+          </Center>
         </Box>
-
-        <Box></Box>
+        <Box>
+          <h1 className="title">Interests</h1>
+          <h1 className="body"></h1>
+          <h1 className="body"> </h1>
+        </Box>
+        <Box>
+          <h1 className="title">Education: </h1>
+          <h1 className="body">University of Califronia, Merced</h1>
+          <h1 className="body">B.S Computer Science and Enginnering </h1>
+        </Box>
+        <Box>
+          <Center>
+            <a href="https://engineering.ucmerced.edu/" target={'_blank'}>
+              <Image
+                className="img"
+                objectFit="scale-down"
+                src="https://caltrout.org/wp-content/uploads/2019/08/UC-merced-logo.png"
+                alt="UCMlogo.js"
+              />
+            </a>
+          </Center>
+        </Box>
+        <Box>hi</Box>
+        <Box>hello </Box>
       </SimpleGrid>
-      <Flex color="white" className="buttons">
+
+      <SimpleGrid color="white" className="buttons">
         <Center>
           <a href="https://github.com/ZEKe125" target="_blank">
             <FaGithub className="icon" />
@@ -62,7 +78,7 @@ function Intro() {
             <FaReact className="icon" />
           </a>
         </Center>
-      </Flex>
+      </SimpleGrid>
     </Grid>
   );
 }
