52af5ddc3fef800f8ff5918495630227ab00f529
ZEKe125 <82134360+ZEKe125@users.noreply.github.com>
2022-05-26 21:20:34 -0700

    pojectCards2

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,22 +1,64 @@
-import React from 'react';
-import Link from 'next/link';
+import React, { useState } from 'react';
+import { Link } from '@chakra-ui/react';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+// import Pagination from 'docs/src/modules/components/Pagination';
 import './projects.css';
 
-function ProjectCard({ name, image, desc }) {
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const styles = {
+  root: {
+    position: 'relative',
+  },
+  slide: {
+    padding: 15,
+    minHeight: 100,
+    color: '#fff',
+  },
+  slide1: {
+    backgroundColor: '#FEA900',
+  },
+  slide2: {
+    backgroundColor: '#B3DC4A',
+  },
+  slide3: {
+    backgroundColor: '#6AC0FF',
+  },
+};
+
+function ProjectCard({ name, image, desc, imageArr}) {
+  const [index, setindex] = useState(0);
+
+  const handleChangeIndex = index => {
+    setindex(index);
+  };
+
   return (
     <div className="project-card">
       <div className="image">
-        <img src={image} alt="projectImage" className="project-image" />
+        {/* <img src={image} alt="projectImage" className="project-image" /> */}
+        <div className="marquee">
+          <div className="track">
+            {imageArr?.map((image, i) => (
+              <img src={image} alt="projectImage" className="project-image" />
+            ))}
+          </div>
+        </div>
       </div>
       <div className="card-bottom">
-        <div className="title">
-          <h2>{name}</h2>
-        </div>
+        <h2 className="project-title">
+          <strong>{name}</strong>
+        </h2>
         <div className="project-description">{desc}</div>
       </div>
       <div className="project-links">
-        <Link>Live Project</Link>
-        <Link>Project Code</Link>
+        <Link>
+          <button>Live Project</button>
+        </Link>
+        <Link>
+          <button>Project Code</button>
+        </Link>
       </div>
     </div>
   );
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,8 +2,16 @@ import React from 'react';
 import { Grid, Text, Code } from '@chakra-ui/react';
 import ProjectCard from './projectCard';
 import './projects.css';
-
+import p1i1 from '../../assets/project1/omronProjectMobil (1).png';
+import p1i2 from '../../assets/project1/omronProjectMobil (2).png';
+import p1i3 from '../../assets/project1/omronProjectMobil (3).png';
 function Projects() {
+  const ProjectImages1 = [p1i1, p1i2, p1i3];
+  const ProjectDesc1 =
+    'This Predictive Maintance Dashboard was Developed as Proof of Concept for the Omron Corp. Utilizes React, Flask API, SQLite, d3.js.';
+  const ProjectDesc2 = '';
+  const ProjectDesc3 = '';
+  const ProjectDesc4 = '';
   return (
     <Grid
       className="projects"
@@ -14,6 +22,16 @@ function Projects() {
         <Text>
           <Code>{`<Projects />`}</Code>
         </Text>
+      </div>
+      <div className="project-cards">
+        <ProjectCard
+          name="Predictive Maintanace Dashboard (OMRON Corp)"
+          imageArr={ProjectImages1}
+          desc={ProjectDesc1}
+        />
+        <ProjectCard name="" image={''} desc={ProjectDesc2} />
+        <ProjectCard name="" image={''} desc={ProjectDesc3} />
+        <ProjectCard name="Portfolio Website" image={''} desc={ProjectDesc4} />
       </div>
     </Grid>
   );
